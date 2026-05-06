2b3df3b9312d01b47b05d0f2d963651fddae04a8
abdullahchattha988-hue <abdullahchattha988@gmail.com>
2026-05-06 20:39:26 +0500

    fkwf

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
     react(),
-    tailwindcss(),
     tsConfigPaths(),
   ],
 });
